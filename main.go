96c12f55514e8efc652a33e8825b736f9ae78178
Randall Stroup <Mortimus@gmail.com>
2021-06-13 21:24:11 -0500

    fixed dump date issue

--- main.go
+++ main.go
@@ -845,7 +845,7 @@ var bosses int
 
 func uploadRaidDump(filename string) {
 	file, err := os.Open(configuration.Everquest.BaseFolder + "/" + filename)
-	stamp := time.Now().Format("20160102_")
+	stamp := time.Now().Format("20160102")
 	if err != nil {
 		Err.Printf("Error finding Raid Dump: %s", err.Error())
 		discord.ChannelMessageSend(configuration.Discord.RaidDumpChannelID, "Error uploading Raid Dump: "+filename)
@@ -856,7 +856,7 @@ func uploadRaidDump(filename string) {
 			// Start timer
 			raidStart = getTime().Round(1 * time.Hour)
 			nextDump = raidStart.Add(1 * time.Hour)
-			discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"raid_start", file)
+			discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"_raid_start", file)
 		} else {
 			if needsDump && getTime().Round(1*time.Hour) == nextDump {
 				raidDumps++
@@ -865,12 +865,12 @@ func uploadRaidDump(filename string) {
 				hourly++
 				hString := strconv.Itoa(hourly)
 				DiscordF(configuration.Discord.RaidDumpChannelID, "%s uploaded an hourly raid dump at %s for %s", getPlayerName(configuration.Everquest.LogPath), getTime().String(), currentZone)
-				discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"hour"+hString, file)
+				discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"_hour"+hString, file)
 			} else {
 				bosses++
 				hBosses := strconv.Itoa(bosses)
 				DiscordF(configuration.Discord.RaidDumpChannelID, "%s uploaded a boss kill raid dump at %s for %s", getPlayerName(configuration.Everquest.LogPath), getTime().String(), currentZone)
-				discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"boss"+hBosses, file)
+				discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, stamp+"_boss"+hBosses, file)
 			}
 		}
 		// discord.ChannelFileSend(configuration.Discord.RaidDumpChannelID, filename, file)
